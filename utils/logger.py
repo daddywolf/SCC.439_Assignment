22bc1ede2d6cb4f08d7f8e47a01ddf0bce0998c0
jiangzhipeng <570981884@qq.com>
2022-01-20 20:48:13 +0000

    logger modify

--- utils/logger.py
+++ utils/logger.py
@@ -7,15 +7,14 @@ fh = logging.FileHandler("files/message.log", mode='w')
 
 def log(text):
     def decorator(func):
-        @functools.wraps(func)
         def wrapper(*args, **kw):
             formatter = logging.Formatter("%(asctime)s %(clientip)-15s %(user)-8s %(message)s")
             fh.setLevel(logging.INFO)
             fh.setFormatter(formatter)
             logger.addHandler(fh)
             d = {'clientip': '192.168.0.1', 'user': 'fbloggs'}
-            dd = func.__name__
             logger.warning('Protocol problem: %s', 'connection reset', extra=d)
+            print(args[0][0])
             return func(*args, **kw)
 
         return wrapper
