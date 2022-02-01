74e0cca79ce7b5e64a4c30834faf86fd8a017bb1
jiangzhipeng <570981884@qq.com>
2022-02-01 21:40:08 +0000

    encrypt and decrypt ok. challange ok

--- new_client.py
+++ new_client.py
@@ -7,7 +7,7 @@ from os import urandom
 
 from Cryptodome.Cipher import AES
 from Cryptodome.Hash import SHA256, HMAC
-from Cryptodome.Util.Padding import pad
+from Cryptodome.Util.Padding import pad, unpad
 
 from mycryptolib.lancs_DH import DiffieHellman
 from utils.basic_functions import input_directory, select_user_from_table
@@ -15,13 +15,16 @@ from utils.basic_functions import input_directory, select_user_from_table
 
 class Client:
     def __init__(self, remote_ip, remote_port):
-        self._client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self._client.connect((remote_ip, int(remote_port)))
-        self._current_state = 'init'
+        self._rempte_ip = remote_ip
+        self._remote_port = remote_port
 
     def client_send_message(self, pdu_dict):
+        self._client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._client.connect((self._rempte_ip, int(self._remote_port)))
+        self._current_state = 'init'
         self._client.sendall(json.dumps(pdu_dict).encode())
-        data = self._client.recv(10240)
+        data = self._client.recv(1024)
+        self._client.close()
         return data.decode()
 
     def init(self):
@@ -53,14 +56,15 @@ class Client:
         self._hmac_key = hash.digest()
         hash.update(self._hmac_key)
         self._chap_secret = hash.digest()
-        print(f"Shared Secret Bytes: {self._client_dh.shared_secret_bytes} \n"
-              f"Enc Key :{self._enc_key} \n"
-              f"IV: {self._iv} \n"
-              f"HMAC Key: {self._hmac_key} \n"
-              f"CHAP Secret: {self._chap_secret}")
+        print('Key Generation Successfully')
 
     def hello(self):
-        return self._generate_pdu('hello', None)
+        ret = self.client_send_message(self._generate_pdu('hello', None))
+        print(ret)
+        pdu_dict = json.loads(ret)
+        type, pt = self._decrypt_pdu(pdu_dict)
+        print(f"type: {type}, pt: {pt}")
+        return
 
     def resp(self):
         """
@@ -78,25 +82,48 @@ class Client:
     def text(self):
         return self._generate_pdu('text', 'message'.encode('utf-8'))
 
-    def _generate_pdu(self, state, data):
+    def _generate_pdu(self, msg_type, data):
+        print(f"Plain Text: {data}")
         body = None
         if data:
             cipher = AES.new(self._enc_key, AES.MODE_CBC, self._iv)
             ct_bytes = cipher.encrypt(pad(data, AES.block_size))
             body = base64.b64encode(ct_bytes).decode('utf-8')
-        pdu = {'header': {'msg_type': state, 'timestamp': time.time(), 'crc': 0x00}, 'body': body,
+        pdu = {'header': {'msg_type': msg_type, 'timestamp': time.time(), 'crc': 0x00}, 'body': body,
                'security': {'hmac': {'type': 'SHA256', 'val': 0x00}, 'enc_type': 'AES256-CBC'}}
-        pdu['security']['hmac']['val'] = base64.b64encode(HMAC.new(self._hmac_key, json.dumps(pdu).encode('utf-8'),
-                                                                   digestmod=SHA256).digest()).decode()
+        ct_HMAC = HMAC.new(self._hmac_key, json.dumps(pdu).encode('utf-8'), digestmod=SHA256)
+        pdu['security']['hmac']['val'] = base64.b64encode(ct_HMAC.digest()).decode()
         pdu['header']['crc'] = zlib.crc32(json.dumps(pdu).encode('utf-8'))
         return pdu
 
+    def _decrypt_pdu(self, pdu_dict):
+        crc_other = pdu_dict['header'].pop('crc')
+        pdu_dict['header']['crc'] = 0x00
+        crc_my = zlib.crc32(json.dumps(pdu_dict).encode('utf-8'))
+        if not crc_other == crc_my:
+            raise Exception("CRC ERROR")
+        hmac_other = pdu_dict['security']['hmac'].pop('val')
+        pdu_dict['security']['hmac']['val'] = 0x00
+        hmac_my = HMAC.new(self._hmac_key, json.dumps(pdu_dict).encode('utf-8'), digestmod=SHA256)
+        try:
+            hmac_my.verify(base64.b64decode(hmac_other))
+        except Exception as e:
+            print('       HMAC OK: False')
+        if pdu_dict['body']:
+            ct_bytes = base64.b64decode(pdu_dict['body'])
+            decipher = AES.new(self._enc_key, AES.MODE_CBC, self._iv)
+            pt = unpad(decipher.decrypt(ct_bytes), AES.block_size)
+            return pdu_dict['header']['msg_type'], pt
+        else:
+            return pdu_dict['header']['msg_type'], None
+
 
 if __name__ == "__main__":
+    # Pick User
     directory_dict = input_directory('directory.json')
     user = select_user_from_table(directory_dict)
-    print(user)
-    user = {"username": "jiangzhipeng", "password": "666666", "port": "8888", "ip": "127.0.0.1"}
+    # Init Client
     client = Client(remote_ip='0.0.0.0', remote_port=8888)
     client.init()
     dh_1 = client.dh_1(user)
+    client.hello()
--- new_server.py
+++ new_server.py
@@ -8,7 +8,7 @@ from os import urandom
 
 from Cryptodome.Cipher import AES
 from Cryptodome.Hash import SHA256, HMAC
-from Cryptodome.Util.Padding import pad
+from Cryptodome.Util.Padding import pad, unpad
 
 from mycryptolib.lancs_DH import DiffieHellman
 
@@ -24,6 +24,10 @@ class Server:
         self._iv = b'\xb7N\xa9\xe7\xeau*\x1f\xbc\x86\x0c\xe0xy\xe5\xdc'
         self._hmac_key = b'\x0e\xe6\xcd\xa4vV\xa5\xc8?\xec8\xe2\xffj\xf7k\xef\xd67d\xecJ\xee\xae&:\x11C*\x10N\x1a'
         self._chap_secret = b'\x87\x02x\xa3\xe9\xf1p\x17_`\xcc\xf1\xb9\xaf\xc7LqH\xb1\xdc\xc1\xe9\x9f|\x8f\xc6\x16}*\xf4\xf3&'
+
+    @property
+    def server(self):
+        return self._server
 
     def server_send_message(self, pdu_dict):
         conn, addr = self._server.accept()
@@ -45,7 +49,7 @@ class Server:
         # DO DH KEY EXCHANGE
         conn, addr = self._server.accept()
         print('Connected by', addr)
-        data = conn.recv(10240)
+        data = conn.recv(1024)
         dh_1_pdu = json.loads(data.decode())
         username = dh_1_pdu['body']['user']
         print(username)
@@ -54,6 +58,7 @@ class Server:
                     'body': {'key': base64.b64encode(str(self._public_key).encode()).decode('utf-8')}}
         dh_2_pdu['header']['crc'] = zlib.crc32(json.dumps(dh_2_pdu).encode('utf-8'))
         conn.sendall(json.dumps(dh_2_pdu).encode())
+        conn.close()
         self._server_dh.generate_shared_secret(client_public_key)
         # CALCULATE KEYS
         directory = open('../files/directory.json', 'r')
@@ -71,14 +76,18 @@ class Server:
         self._hmac_key = hash.digest()
         hash.update(self._hmac_key)
         self._chap_secret = hash.digest()
-        print(f"Shared Secret Bytes: {self._server_dh.shared_secret_bytes} \n"
-              f"Enc Key :{self._enc_key} \n"
-              f"IV: {self._iv} \n"
-              f"HMAC Key: {self._hmac_key} \n"
-              f"CHAP Secret: {self._chap_secret}")
+        print('Key Generation Successfully')
+        return True
 
     def chall(self):
-        return self._generate_pdu('chall', urandom(32))
+        conn, addr = self._server.accept()
+        print('Connected by', addr)
+        data = conn.recv(1024)
+        type, pt = self._decrypt_pdu(json.loads(data.decode()))
+        if type == 'hello':
+            conn.sendall(json.dumps(self._generate_pdu('chall', urandom(32))).encode())
+        conn.close()
+        return True
 
     def resp(self):
         """
@@ -96,21 +105,45 @@ class Server:
         print('end')
         sys.exit(0)
 
-    def _generate_pdu(self, state, data):
+    def _generate_pdu(self, msg_type, data):
+        print(f"Plain Text: {data}")
         body = None
         if data:
             cipher = AES.new(self._enc_key, AES.MODE_CBC, self._iv)
             ct_bytes = cipher.encrypt(pad(data, AES.block_size))
             body = base64.b64encode(ct_bytes).decode('utf-8')
-        pdu = {'header': {'msg_type': state, 'timestamp': time.time(), 'crc': 0x00}, 'body': body,
+        pdu = {'header': {'msg_type': msg_type, 'timestamp': time.time(), 'crc': 0x00}, 'body': body,
                'security': {'hmac': {'type': 'SHA256', 'val': 0x00}, 'enc_type': 'AES256-CBC'}}
-        pdu['security']['hmac']['val'] = base64.b64encode(HMAC.new(self._hmac_key, json.dumps(pdu).encode('utf-8'),
-                                                                   digestmod=SHA256).digest()).decode()
+        ct_HMAC = HMAC.new(self._hmac_key, json.dumps(pdu).encode('utf-8'), digestmod=SHA256)
+        pdu['security']['hmac']['val'] = base64.b64encode(ct_HMAC.digest()).decode()
         pdu['header']['crc'] = zlib.crc32(json.dumps(pdu).encode('utf-8'))
         return pdu
 
+    def _decrypt_pdu(self, pdu_dict):
+        crc_other = pdu_dict['header'].pop('crc')
+        pdu_dict['header']['crc'] = 0x00
+        crc_my = zlib.crc32(json.dumps(pdu_dict).encode('utf-8'))
+        if not crc_other == crc_my:
+            raise Exception("CRC ERROR")
+        hmac_other = pdu_dict['security']['hmac'].pop('val')
+        pdu_dict['security']['hmac']['val'] = 0x00
+        hmac_my = HMAC.new(self._hmac_key, json.dumps(pdu_dict).encode('utf-8'), digestmod=SHA256)
+        try:
+            hmac_my.verify(base64.b64decode(hmac_other))
+        except Exception as e:
+            print('       HMAC OK: False')
+        if pdu_dict['body']:
+            ct_bytes = base64.b64decode(pdu_dict['body'])
+            decipher = AES.new(self._enc_key, AES.MODE_CBC, self._iv)
+            pt = unpad(decipher.decrypt(ct_bytes), AES.block_size)
+            return pdu_dict['header']['msg_type'], pt
+        else:
+            return pdu_dict['header']['msg_type'], None
+
 
 if __name__ == "__main__":
+    # Init Server
     server = Server(local_port=8888)
     server.init()
     dh_2 = server.dh_2()
+    chall = server.chall()
