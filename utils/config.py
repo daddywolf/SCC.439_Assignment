17eeffde85187608b4c9445d1336321d7c9f586f
jiangzhipeng <570981884@qq.com>
2022-02-03 23:27:26 +0000

    logs

--- utils/config.py
+++ utils/config.py
@@ -12,5 +12,6 @@ By Zhipeng Jiang @Lancaster University     V1.0
 PATH = f"{os.getcwd()}/files/"
 LOG_FILE = 'secure_log.log'
 
+# THE LOG_KEY AND LOG_PASSWORD SHOULD BE STORED CAREFULLY! (I included this as a vulnerability to the vulnerability text documentation.)
 LOG_KEY = b'O\xb9\xa8q\xa1\x97\xb9\x00\xc4\x81\x88\x1f6\xf2Q\xf1\xfa\xc8\x9f\x12\xc13{ X\x8a3\x93\x0e\xfd\x17\x1b'
 LOG_PASSWORD = b'I WANT HIGHER SCORE!'
--- utils/secure_logging.py
+++ utils/secure_logging.py
@@ -8,7 +8,15 @@ from utils.config import LOG_PASSWORD, LOG_KEY, LOG_FILE, PATH
 
 
 class SecureLogging:
+    """
+    This class is for encrypted logging.
+    """
+
     def __init__(self, filename):
+        """
+        Constructor. Generates all required keys from config.py (LOG_PASSWORD and LOG_KEY)
+        :param filename: the log file you want to write.
+        """
         self._file = open(PATH + filename, 'a')
         self._hmac = HMAC.new(LOG_PASSWORD, LOG_KEY, digestmod=SHA256)
         self._enc_key = self._hmac.digest()
@@ -17,27 +25,47 @@ class SecureLogging:
         self._iv = self._hash.digest()[:16]
 
     def _log_encryption(self, data):
+        """
+        This function is the inverse function of _log_decryption.
+        Encrypt message from plain text to cipher text
+        :param data: plain text
+        :return: cipher text
+        """
         data = data.encode()
         cipher = AES.new(self._enc_key, AES.MODE_CBC, self._iv)
         ct_bytes = cipher.encrypt(pad(data, AES.block_size))
         return ct_bytes
 
     def _log_decryption(self, data):
+        """
+        This function is the inverse function of _log_encryption.
+        Decrypt message from cipher text to plain text
+        :param data: cipher text
+        :return: plain text
+        """
         data = bytes(data)
         decipher = AES.new(self._enc_key, AES.MODE_CBC, self._iv)
         pt = unpad(decipher.decrypt(data), AES.block_size)
         return pt.decode()
 
     def log(self, level, message):
+        """
+        Write the encrypted and HMACed message to log file
+        :param level: log level.
+        :param message: plain text message
+        """
         timestamp = int(time.time())
         message_format = f"{timestamp}::{level}::{message[0]['username']}::{self._log_encryption(message[1])}"
         self._hmac.update(message_format.encode())
         log_format = message_format + f"::{self._hmac.hexdigest()}\n"
         self._file.writelines(log_format)
-        return log_format
 
     def read_logs(self, filename):
-        self._file = open(PATH+filename, 'r')
+        """
+        Show the decrypted log message from the encrypted message.
+        :param filename: the log file you want to read.
+        """
+        self._file = open(PATH + filename, 'r')
         count = 0
         while 1:
             count += 1
@@ -58,10 +86,11 @@ class SecureLogging:
 
 
 def log_to_file(level, message):
+    """
+    Simple log functions to log messages from user input.
+    :param level: log level.
+    :param message: plain text message
+    :return:
+    """
     sl = SecureLogging(LOG_FILE)
     sl.log(level, message)
-
-
-if __name__ == "__main__":
-    sl = SecureLogging(LOG_FILE)
-    sl.read_logs(LOG_FILE)
